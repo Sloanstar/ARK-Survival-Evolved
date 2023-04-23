0714d2c399d7f7356143b54d6b18a168f6397ee5
Sloanstar <jason.a.sloan@gmail.com>
2023-04-22 21:31:45 -0400

    Update entry.sh

--- bullseye/etc/entry.sh
+++ bullseye/etc/entry.sh
@@ -25,19 +25,21 @@ echo "Clearing Mods..."
 # remove the workshop mods
 find "${MODPATH}"/* -maxdepth 0 -regextype posix-egrep -regex ".*/[[:digit:]]+" | xargs -0 -d"\n" rm -R 2>/dev/null
 
+#Comment out SQUAD mod logic, evaluate for ARK later
 # Install mods (if defined)
 declare -a MODS="${MODS}"
-if (( ${#MODS[@]} ))
-then
-	echo "Installing Mods..."
-	for MODID in "${MODS[@]}"; do
-		echo "> Install mod '${MODID}'"
-		"${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" +login anonymous +workshop_download_item "${WORKSHOPID}" "${MODID}" +quit
-
-		echo -e "\n> Link mod content '${MODID}'"
-		ln -s "${STEAMAPPDIR}/steamapps/workshop/content/${WORKSHOPID}/${MODID}" "${MODPATH}/${MODID}"
-	done
-fi
+#if (( ${#MODS[@]} ))
+#then
+#	echo "Installing Mods..."
+#	for MODID in "${MODS[@]}"; do
+#		echo "> Install mod '${MODID}'"
+#		"${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" +login anonymous +workshop_download_item "${WORKSHOPID}" "${MODID}" +quit
+#
+#		echo -e "\n> Link mod content '${MODID}'"
+#		ln -s "${STEAMAPPDIR}/steamapps/workshop/content/${WORKSHOPID}/${MODID}" "${MODPATH}/${MODID}"
+#	done
+#fi
 
 ulimit -n 1000000
+echo App dir: "${STEAMAPPDIR}"
 bash -c "${STEAMAPPDIR}/ShooterGame/Binaries/Linux/ShooterGameServer" "${MAPNAME}"?listen?SessionName="${SESSIONNAME}"?Port="${PORT}"?QueryPort="${QUERYPORT}"?RCONPORT="${RCONPORT}" -server -log -crossplay
