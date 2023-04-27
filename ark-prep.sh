ce130da70fcb28b9e681e0261c7c127887c700ab
root <root@lxc-deb-docker02.8.8.8.8>
2023-04-27 22:16:15 +0000

    docker-compose example change to NFS Volume for cluster. changes to ark-prep.sh

--- ark-prep.sh
+++ ark-prep.sh
@@ -1,14 +1,20 @@
 ARKROOT='/opt'
 ARKSERVER=${ARKROOT}'/ARK-Server'
 ARKCLUSTER=${ARKROOT}'/ARK-Cluster'
-STEAM='/opt/Steam'
+STEAM=${ARKROOT}'/Steam'
+declare -a BASEDIRS=( "${ARKSERVER}" "${ARKCLUSTER}" "${STEAM}" )
 echo Making Server Directories...
-mkdir -p "${ARKSERVER}" "${ARKCLUSTER}" "${STEAM}"
-chown -R nobody:nogroup "${ARKSERVER}" "${ARKCLUSTER}" "${STEAM}"
-chmod -R 777 "${ARKSERVER}" "${ARKCLUSTER}" "${STEAM}"
+for DIR in "${BASEDIRS[@]}"; do
+	echo Creating "${DIR}"
+	mkdir -p "${DIR}"
+	echo "Setting owner nobody:nogroup"
+	chown -R nobody:nogroup "${DIR}"
+	echo "Setting permissions to 777"
+	chmod -R 777 "${DIR}"
+done
 
 echo Building Instance Structure...
-ARKINSTANCES=('ARK101' 'ARK102' 'ARK103')
+ARKINSTANCES=('ARK100' 'ARK101' 'ARK102')
 for INSTANCE in "${ARKINSTANCES[@]}"; do
 	mkdir -p "${ARKROOT}/${INSTANCE}/Logs" "${ARKROOT}/${INSTANCE}/SavedArks" "${ARKROOT}/${INSTANCE}/Config" "${ARKROOT}/${INSTANCE}/MODS"
 	chown -R nobody:nogroup "${ARKROOT}/${INSTANCE}"
