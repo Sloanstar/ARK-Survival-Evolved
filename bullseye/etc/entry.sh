203ed6437191d038868802c4cc90fe59f23a604a
root <root@lxc-deb-docker02.freedom.local>
2023-04-28 17:34:36 +0000

    refactored entry.sh to accept space separated string - more user friendly.

--- bullseye/etc/entry.sh
+++ bullseye/etc/entry.sh
@@ -27,42 +27,46 @@ echo "Clearing Mods..."
 find "${MODPATH}"/* -maxdepth 0 -regextype posix-egrep -regex ".*/[[:digit:]]+" | xargs -0 -d"\n" rm -R 2>/dev/null
 
 # Install mods (if defined)
-# Set MODS as array
-declare -a MODS="${MODS}"
-# If there are elements in the array
-if (( ${#MODS[@]} ))
+if [ -n "${MODS}" ]
 then
-	echo "Installing Mods..."
-	# For each MOD element in the array
-	for MODID in "${MODS[@]}"; do
-		echo "> Install mod '${MODID}'"
-		# Download the MOD
-		"${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" +login anonymous +workshop_download_item "${WORKSHOPID}" "${MODID}" +quit
+	# Set MODS as array
+	declare -a MODS=("${MODS}")
 
-		echo -e "\n> Link mod content '${MODID}'"
-		# Create a symbolic link from the download to the MOD folder.
-		ln -s "${STEAMAPPDIR}/steamapps/workshop/content/${WORKSHOPID}/${MODID}/" "${MODPATH}/${MODID}"
-	done
-	
-	# Build a CSV string of MODs to use in the server start CLI
-	# Integer equal to the number of elements in MODS - 1
-	declare -i LEN="${#MODS[@]}"-1
-        for (( i=0; i<="${#MODS[@]}"; i++ ))
-        do
-		# Concat MODID to string variable
-                GAMEMODIDS+="${MODS[$i]}"
-		# Check for last element, if not last add a comma.
-                if [ $i -lt $LEN ]
-                then
-			# Concat comma to string variable
-                        GAMEMODIDS+=","
-                fi
-        done
-	# Echo Mods string to console for good measure.
-	echo GameModIds: $GAMEMODIDS
-	
-	echo CLI: "${MAPNAME}"?listen?SessionName="${SESSIONNAME}"?Port="${PORT}"?QueryPort="${QUERYPORT}"?RCONPort="${RCONPORT}"?RCONEnabled=True?ServerAdminPassword="${ADMINPW}"?GameModIds="${GAMEMODIDS}" -NoTransferFromFiltering -clusterid="${CLUSTERKEY}" -ClusterDirOverride="${STEAMAPPDIR}/Cluster" -crossplay -gameplaylogging -UseStructureStasisGrid -lowmemory -ForceRespawnDinos -UseDynamicConfig
-	"${STEAMAPPDIR}"/ShooterGame/Binaries/Linux/ShooterGameServer "${MAPNAME}"?listen?SessionName="${SESSIONNAME}"?Port="${PORT}"?QueryPort="${QUERYPORT}"?RCONPort="${RCONPORT}"?RCONEnabled=True?ServerAdminPassword="${ADMINPW}"?GameModIds="${GAMEMODIDS}" -NoTransferFromFiltering -clusterid="${CLUSTERKEY}" -ClusterDirOverride="${STEAMAPPDIR}/Cluster" -crossplay -gameplaylogging -UseStructureStasisGrid -lowmemory -ForceRespawnDinos -UseDynamicConfig
+	# If there are elements in the array
+	if (( ${#MODS[@]} ))
+	then
+		echo "Installing Mods..."
+		# For each MOD element in the array
+		for MODID in "${MODS[@]}"; do
+			echo "> Install mod '${MODID}'"
+			# Download the MOD
+			"${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" +login anonymous +workshop_download_item "${WORKSHOPID}" "${MODID}" +quit
+
+			echo -e "\n> Link mod content '${MODID}'"
+			# Create a symbolic link from the download to the MOD folder.
+			ln -s "${STEAMAPPDIR}/steamapps/workshop/content/${WORKSHOPID}/${MODID}/" "${MODPATH}/${MODID}"
+		done
+		
+		# Build a CSV string of MODs to use in the server start CLI
+		# Integer equal to the number of elements in MODS - 1
+		declare -i LEN="${#MODS[@]}"-1
+        	for (( i=0; i<="${#MODS[@]}"; i++ ))
+        	do
+			# Concat MODID to string variable
+        	        GAMEMODIDS+="${MODS[$i]}"
+			# Check for last element, if not last add a comma.
+        	        if [ $i -lt $LEN ]
+        	        then
+				# Concat comma to string variable
+        	                GAMEMODIDS+=","
+        	        fi
+        	done
+		# Echo Mods string to console for good measure.
+		echo GameModIds: $GAMEMODIDS
+		
+		echo CLI: "${MAPNAME}"?listen?SessionName="${SESSIONNAME}"?Port="${PORT}"?QueryPort="${QUERYPORT}"?RCONPort="${RCONPORT}"?RCONEnabled=True?ServerAdminPassword="${ADMINPW}"?GameModIds="${GAMEMODIDS}" -NoTransferFromFiltering -clusterid="${CLUSTERKEY}" -ClusterDirOverride="${STEAMAPPDIR}/Cluster" -crossplay -gameplaylogging -UseStructureStasisGrid -lowmemory -ForceRespawnDinos -UseDynamicConfig
+		"${STEAMAPPDIR}"/ShooterGame/Binaries/Linux/ShooterGameServer "${MAPNAME}"?listen?SessionName="${SESSIONNAME}"?Port="${PORT}"?QueryPort="${QUERYPORT}"?RCONPort="${RCONPORT}"?RCONEnabled=True?ServerAdminPassword="${ADMINPW}"?GameModIds="${GAMEMODIDS}" -NoTransferFromFiltering -clusterid="${CLUSTERKEY}" -ClusterDirOverride="${STEAMAPPDIR}/Cluster" -crossplay -gameplaylogging -UseStructureStasisGrid -lowmemory -ForceRespawnDinos -UseDynamicConfig
+	fi
 else
 	echo CLI: "${MAPNAME}"?listen?SessionName="${SESSIONNAME}"?Port="${PORT}"?QueryPort="${QUERYPORT}"?RCONPort="${RCONPORT}"?RCONEnabled=True?ServerAdminPassword="${ADMINPW}" -NoTransferFromFiltering -clusterid="${CLUSTERKEY}" -ClusterDirOverride="${STEAMAPPDIR}/Cluster" -crossplay -gameplaylogging -UseStructureStasisGrid -lowmemory -ForceRespawnDinos -UseDynamicConfig
 	"${STEAMAPPDIR}"/ShooterGame/Binaries/Linux/ShooterGameServer "${MAPNAME}"?listen?SessionName="${SESSIONNAME}"?Port="${PORT}"?QueryPort="${QUERYPORT}"?RCONPort="${RCONPORT}"?RCONEnabled=True?ServerAdminPassword="${ADMINPW}" -NoTransferFromFiltering -clusterid="${CLUSTERKEY}" -ClusterDirOverride="${STEAMAPPDIR}/Cluster" -crossplay -gameplaylogging -UseStructureStasisGrid -lowmemory -ForceRespawnDinos -UseDynamicConfig
