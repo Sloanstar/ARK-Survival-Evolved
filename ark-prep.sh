7ab9b057549081a758c502480bfec558d1877e12
sloanstar <user@host.com>
2024-02-09 23:37:01 +0000

    Folder rescructure to fix .pid errors

--- ark-prep.sh
+++ ark-prep.sh
@@ -2,7 +2,8 @@ ARKROOT='/opt'
 ARKSERVER=${ARKROOT}'/ARK-Server'
 ARKCLUSTER=${ARKROOT}'/ARK-Cluster'
 STEAM=${ARKROOT}'/Steam'
-declare -a BASEDIRS=( "${ARKSERVER}" "${ARKCLUSTER}" "${STEAM}" )
+ARKMANAGER=${ARKROOT}'/arkmanager'
+declare -a BASEDIRS=( "${ARKSERVER}" "${ARKCLUSTER}" "${STEAM}" "${ARKMANAGER}" )
 echo "ARK Instances: $ARKINSTANCES"
 #give opportunity to set ARK Instances in shell - if not build it.
 if  ( test -n "${ARKINSTANCES}" )
@@ -24,6 +25,7 @@ for DIR in "${BASEDIRS[@]}"; do
 	setfacl -d -m g::rwx "${DIR}"
 	setfacl -d -m o::rwx "${DIR}"
 done
+cp bullseye/etc/arkmanager.cfg "${ARKMANAGER}"
 
 echo Building Instance Structure...
 for INSTANCE in ${ARKINSTANCES[@]}; do
@@ -32,9 +34,11 @@ for INSTANCE in ${ARKINSTANCES[@]}; do
         setfacl -d -m u::rwx "${DIR}"
         setfacl -d -m g::rwx "${DIR}"
         setfacl -d -m o::rwx "${DIR}"
-	mkdir -p "${ARKROOT}/${INSTANCE}/Logs" "${ARKROOT}/${INSTANCE}/SavedArks" "${ARKROOT}/${INSTANCE}/Config" "${ARKROOT}/${INSTANCE}/MODS"
+	mkdir -p "${ARKROOT}/${INSTANCE}/Saved/Logs" "${ARKROOT}/${INSTANCE}/Saved/SavedArks" "${ARKROOT}/${INSTANCE}/Saved/Config" "${ARKROOT}/${INSTANCE}/MODS" "${ARKROOT}/${INSTANCE}/etc/arkmanager/instances"
 	chown -R 1000:1000 "${ARKROOT}/${INSTANCE}"
         chmod -R ugo+srwx "${ARKROOT}/${INSTANCE}"
 	cp bullseye/etc/GameUserSettings.ini "${ARKROOT}/${INSTANCE}/Config"
+        ln "${ARKMANAGER}"/arkmanager.cfg -t "${ARKROOT}/${INSTANCE}/etc/arkmanager/"
+        cp bullseye/etc/main.cfg "${ARKROOT}/${INSTANCE}/etc/arkmanager/instances"
 done
 echo "Complete"
